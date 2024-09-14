f1d2faf7e5aca33baca730a865f6a24a47251550
oliveiranunesgabriel <gabrieloliveiranunes22@gmail.com>
2024-09-14 16:33:45 -0300

    Initial commit for iot-router-backend

---
+++ src/main/java/br/com/ufu/iot_router_backend/IotRouterBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.ufu.iot_router_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotRouterBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IotRouterBackendApplication.class, args);
+	}
+
+}
